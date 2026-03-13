609f41ef6f068d64041ac6ea1daff11cf7638a09
rudra <bhupandertech@gmail.com>
2026-03-13 12:25:57 +0530

    Implemented React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 // src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
-import VideoGrid from './components/VideoGrid';
+import Home from './pages/Home';
+import VideoPlayer from './pages/VideoPlayer';
+import Auth from './pages/Auth';
+import Channel from './pages/Channel';
 
 function App() {
-  // State to toggle the sidebar from the hamburger menu
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   const toggleSidebar = () => {
@@ -13,20 +16,24 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col h-screen bg-white">
-      {/* Header section with hamburger toggle */}
-      <Header toggleSidebar={toggleSidebar} />
-      
-      <div className="flex flex-1 overflow-hidden">
-        {/* Sidebar */}
-        <Sidebar isOpen={isSidebarOpen} />
+    <Router>
+      <div className="flex flex-col h-screen bg-white">
+        <Header toggleSidebar={toggleSidebar} />
         
-        {/* Main Content Area */}
-        <main className="flex-1 overflow-y-auto bg-gray-50 p-4">
-          <VideoGrid />
-        </main>
+        <div className="flex flex-1 overflow-hidden">
+          <Sidebar isOpen={isSidebarOpen} />
+          
+          <main className="flex-1 overflow-y-auto bg-gray-50 p-4">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/video/:videoId" element={<VideoPlayer />} />
+              <Route path="/auth" element={<Auth />} />
+              <Route path="/channel/:channelId" element={<Channel />} />
+            </Routes>
+          </main>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
